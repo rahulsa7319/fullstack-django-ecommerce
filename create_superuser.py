3183fe0eee5d4af9d054208d0da5450d207f063a
Rahul Samanta <rahulsamanta7319@gmail.com>
2026-04-12 09:56:29 +0530

    Add auto superuser script

--- create_superuser.py
+++ create_superuser.py
@@ -3,4 +3,4 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'admin@gmail.com', 'admin123')
+    User.objects.create_superuser('rahul', 'rahulsamanta7319@gmail.com', 'Rahul@7319')
